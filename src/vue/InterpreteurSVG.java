e54555e94b881857d6bcc708aa33054f996169bd
unknown <csylla@ITEM-63630.dhcp.ntes.fr.sopra>
2014-11-06 12:52:23 +0100

    IntepreteurSVG

--- src/vue/InterpreteurSVG.java
+++ src/vue/InterpreteurSVG.java
@@ -1,6 +1,9 @@
 package vue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 import langage.Dessin;
 
@@ -9,8 +12,28 @@ public class InterpreteurSVG implements Interpreteur {
 	@Override
 	public File interpreter(Dessin d) {
 		// TODO Auto-generated method stub
-		File file = new File("Dessin interprete");
+		File file = new File("DessinInterprete.svg");
+		
+		try {
+			PrintWriter ecrire = new PrintWriter(new FileOutputStream("DessinInterprete.svg"));
+			ecrire.println("<?xml version='1.0'?>");
+			ecrire.println("<svg xmlns='http://www.w3.org/2000/svg' width='550' height='300'>");
+			
+			// Ecrire ici le dessin
+			
+			ecrire.println("</svg");
+			ecrire.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		return file;
+	}
+	
+	public static void main(String[] args) {
+		InterpreteurSVG intSVG = new InterpreteurSVG();
+		Dessin dessin = new Dessin("Exemple");
+		intSVG.interpreter(dessin);
 	}
 
 }
